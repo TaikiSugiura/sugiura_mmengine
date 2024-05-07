834f6d0c0bce4ceebc662a9505f2e34f318a27ff
Taiki Sugiura <t.sugiura.108@nitech.jp>
2024-05-07 18:05:44 +0900

    test

--- mmengine/runner/runner.py
+++ mmengine/runner/runner.py
@@ -459,7 +459,7 @@ class Runner:
             Runner: A runner build from ``cfg``.
         """
         cfg = copy.deepcopy(cfg)
-        print('aaaaaaaaaaaaaaaaaaaaba')
+        print('cccccccccccccccccccccccccc')
         runner = cls(
             model=cfg['model'],
             work_dir=cfg['work_dir'],
